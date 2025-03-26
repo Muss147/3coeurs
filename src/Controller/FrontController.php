d5ad91f4e5cfb1148b64bb3a5c489903c0f12e73
Moussa Fofana <mfofana@aguimaweb.com>
2025-03-26 18:30:50 +0000

    Contruction osature 2%

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class FrontController extends AbstractController
+{
+    public function header(): Response
+    {
+        return $this->render('front/_header.html.twig');
+    }
+
+    #[Route('/', name: 'app_front')]
+    public function index(): Response
+    {
+        return $this->render('front/index.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+}
